655c428a61db567499e539f921dc0a19a4fe3239
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-04-01 02:55:41 +0100

    feat: add pokemon fetch utility function

---
+++ src/utils/fetchCards.js
@@ -0,0 +1,24 @@
+export default async function fetchCards() {
+  const cardIds = [];
+
+  while (cardIds.length < 12) {
+    const randomId = Math.floor(Math.random() * 1010 + 1);
+    if (!cardIds.includes(randomId)) {
+      cardIds.push(randomId);
+    }
+  }
+
+  const data = await Promise.all(
+    cardIds.map((id) => fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)),
+  );
+
+  const jsonData = await Promise.all(data.map((response) => response.json()));
+
+  const cards = jsonData.map(({ id, name, sprites: { front_default } }) => ({
+    id,
+    name,
+    front_default,
+  }));
+
+  return cards;
+}
